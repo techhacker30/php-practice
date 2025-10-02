b02fcb2fe7d7a5b5339686ecbb6df3fb76342331
techhacker30 <rohankakde27@gamil.com>
2025-10-03 05:12:36 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MYPHPFILE</title>
+</head>
+<body>
+    <?php
+        echo "This is my first php file";
+    ?>
+</body>
+</html>
