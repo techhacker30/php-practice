f2364ce8d473bb20b6a302ec4899f05823226f63
techhacker30 <rohankakde27@gamil.com>
2025-10-05 01:42:35 +0530

    successfully maked task adding feture

--- to-do-app/database.php
+++ to-do-app/database.php
@@ -10,5 +10,16 @@ function make_connection()
     if($con->connect_error){
         echo "Ther is an erro while connecting";
     }
+    return $con;
+}
+
+function add_task($task){
+    // echo "Adding Your Tas k".$task;
+    $con = make_connection();
+    $query = "INSERT INTO tasks(id, task, status) VALUE(NULL, '$task', 'Not Complited')";
+    $con->query($query);
+}
+function remove_task($id){
+
 }
 ?>
--- to-do-app/handelaction.php
+++ to-do-app/handelaction.php
@@ -1,16 +1,27 @@
-<!-- import database -->
 <?php
+//  import database 
+include("database.php");
 
 if($_SERVER["REQUEST_METHOD"]=="POST"){
     if(isset($_POST["add"])){
         $task = $_POST['task'];
-        echo "added ".$task;
+        // echo "added ".$task;
+        if($_POST['task']){
+            add_task($_POST['task']);
+        }
     }
     if(isset($_POST["remove"])){
-        echo "remove";
+        // echo "remove";
+        if($_POST['remove']){
+            remove_task($_POST['remove']);
+        }
     }
     if(isset($_POST["complite"])){
-        echo "complite";
+        // echo "complite";
+        if($_POST['complite']){
+            task_status($_POST['complite']);
+        }
     }
+    header("Location: index.html");
 }
 ?>
