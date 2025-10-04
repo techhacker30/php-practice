ef0260a08deb63917dab9b954fde0a8522ace8b2
techhacker30 <rohankakde27@gamil.com>
2025-10-05 00:23:46 +0530

    Adding buttons

---
+++ to-do-app/script.js
@@ -0,0 +1,5 @@
+function addtask() {
+    
+    // document.getElementsByClassName('list').innerHTML = "<h1>This is javascrip power</h1>";
+    document.getElementsByTagName('ol').innerHTML = "<h1>this is content</h1>";
+}
