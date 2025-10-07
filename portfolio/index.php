e1811f7b913a043a3d4465e7cf59d5680eefc7c4
techhacker30 <rohankakde27@gamil.com>
2025-10-07 22:52:09 +0530

    adding db code

--- portfolio/index.php
+++ portfolio/index.php
@@ -1,18 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MYPHPFILE</title>
-</head>
-<body>
-    <?php
-        echo "This is my first php file";
-    ?>
-</body>
-</html>
-</html>
 <?php
+    $server = "localhost";
+    $user = "root";
+    $password = "";
+    $db = "test";
+
+    $con = new mysqli($server, $user, $password, $db);
     $name = $_GET['name'];
     $birth = $_GET['bdate'];
     $email = $_GET['email'];
